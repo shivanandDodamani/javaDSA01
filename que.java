f69ce76c7be9f52cb9c2805cbb17a0d14a7bdb52
shivanandDodamani <shivananddodamani635@gmail.com>
2025-12-14 22:36:04 +0530

    question problem

---
+++ que.java
@@ -0,0 +1,10 @@
+
+
+public class que {
+    
+    public static void main (String args[]) {
+        for (int counter = 0; counter < 3; counter = counter + 1) {
+            System.out.println("hello world");
+        }
+    }
+}
