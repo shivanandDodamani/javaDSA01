89d63be3bb0e13e4d54ab097b13b72203052b981
shivanandDodamani <shivananddodamani635@gmail.com>
2025-12-18 23:39:02 +0530

    builded calculator

---
+++ video3/calculator.java
@@ -0,0 +1,39 @@
+package video3;
+
+import java.util.*;
+
+public class calculator {
+    public static void main ( String args[]) {
+        Scanner sc = new Scanner (System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int operator = sc.nextInt();
+
+        switch (operator) {
+            case 1: System.out.println(a+b);
+                break;
+            case 2: System.out.println(a-b);
+                break;
+            case 3:  System.out.println(a*b);
+                break;
+            case 4:if (b==0) {
+                System.out.print("invalid Divison");
+            } else {
+                System.out.println(a/b);
+            }
+            break;
+            case 5: if (b==0) {
+                System.out.println("invalid Divison");
+            } else {
+                System.out.println(a%b);
+            }
+            break;
+            default:System.out.println("invalid opretor");
+            
+        }
+        
+
+    }
+    
+}
