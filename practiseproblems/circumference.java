9a3be38daeb4d22827ba24859057db01d917a5fa
shivanandDodamani <shivananddodamani635@gmail.com>
2026-01-02 22:49:57 +0530

    solved problem

---
+++ practiseproblems/circumference.java
@@ -0,0 +1,16 @@
+package practiseproblems;
+
+import java.util.*;
+
+public class circumference {
+    public static void circumferenceofCircle( int r) {
+        double circumference = 2 * 3.14 * r;
+        System.out.println("circumference of circle is" + circumference);
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextDouble();
+        circumferenceofCircle((int)r);
+    }
+    
+}
--- practiseproblems/sumofodd.java
+++ practiseproblems/sumofodd.java
@@ -1,13 +1,24 @@
 package practiseproblems;
 
+import java.util.*;
+
 public class sumofodd {
     public static void sumofOdd(int n) {
         int sum = 0;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             if ( i%2 != 0) {
-                sum = sum + 1;
+                sum = sum + i;
             }
         }
+            System.out.println(sum);
+        
+    }
+        public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sumofOdd(n);
+            
+        
     }
     
 }
