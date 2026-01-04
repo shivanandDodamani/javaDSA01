f58fabff01aafa885c753fd991ff383eeed2fc58
shivanandDodamani <shivananddodamani635@gmail.com>
2026-01-04 22:41:23 +0530

    solved ques

---
+++ practiseproblems/dowhile.java
@@ -0,0 +1,22 @@
+package practiseproblems;
+
+public class dowhile {
+       public static void main(String args[]) {
+      class InfiniteLoop {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        do {
+            System.out.println("Infinite loop");
+        } while (i > 0);
+
+    }
+}
+  
+    }
+}
+
+    
+    
+
---
+++ practiseproblems/lastques.java
@@ -0,0 +1,13 @@
+package practiseproblems;
+
+import java.util.Scanner;
+
+public class lastques {
+    public static void main(String args[]) {
+       int positive = 0, negative = 0, zeros = 0;
+       System.out.println("Press 1 to continue & 0 to stop");
+       Scanner sc = new Scanner(System.in);
+       int input = sc.nextInt();
+    
+}
+}
