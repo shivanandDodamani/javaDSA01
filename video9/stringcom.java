a32fa97adb38b7079df819e2a1ce5777ee6ef492
shivanandDodamani <shivananddodamani635@gmail.com>
2026-01-16 19:15:51 +0530

    Strings compare

---
+++ video9/stringcom.java
@@ -0,0 +1,19 @@
+package video9;
+
+import java.util.*;
+
+public class stringcom {
+    public static void main(){
+        String name1 = "Shivanand"; 
+        String name2 = "Dodamani";
+        String fullName = name1+name2;
+
+        if(name1.compareTo(name2) == 0) {
+            System.out.println("strings are equal");
+        } else {
+            System.out.println("Strings is not equal");
+        }
+
+    }
+    
+}
