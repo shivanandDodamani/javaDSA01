03aca000fe320b4a549818420054938544d0f72b
shivanandDodamani <shivananddodamani635@gmail.com>
2026-01-17 18:14:57 +0530

    cumulative concept

---
+++ video9/cumulative.java
@@ -0,0 +1,21 @@
+package video9;
+
+import java.util.*;
+
+public class cumulative {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+          int size = sc.nextInt();
+        String array[] = new String[size];
+        int totLength = 0;
+        
+        for(int i = 0; i < size; i++){
+            array[i] = sc.next();
+            totLength += array[i].length();
+            
+        }
+        System.out.println(totLength);
+
+    }
+    
+}
