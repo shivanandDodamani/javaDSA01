7f6e4b6a5c1c922436da6db5ae33db4c6e0bc103
shivanandDodamani <shivananddodamani635@gmail.com>
2026-01-18 20:35:56 +0530

    substring

---
+++ video9/substring.java
@@ -0,0 +1,14 @@
+package video9;
+
+import java.util.*;
+
+public class substring {
+    public static void main() {
+        
+        String sentence = "Shivanand";
+        String name = sentence.substring(4);
+        System.out.println(name);
+
+    }
+    
+}
