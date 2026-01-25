2f64a6ccf2587c03786e8160468149e6ad027ed6
shivanandDodamani <shivananddodamani635@gmail.com>
2026-01-25 11:52:00 +0530

    operators in java

---
+++ video11/operators.java
@@ -0,0 +1,18 @@
+package video11;
+
+public class operators {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 0;
+
+        //b = a++;
+        //b = ++a;
+        //b = --a;
+        //b = a--;
+   
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+    
+}
