d6d097d48010dcf5e6b622a4dbc16bf9cbe8c072
shivanandDodamani <shivananddodamani635@gmail.com>
2026-02-23 21:41:30 +0530

    using functions in java

--- vedio6/functions.java
+++ vedio6/functions.java
@@ -20,7 +20,7 @@ public class functions {
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
             printFactorial(n);
-            
+           sc.close(); 
         }
     
     
