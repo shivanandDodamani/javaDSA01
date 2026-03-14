95693c02583d0aa14572b87822e7dacb2f751f93
shivanandDodamani <shivananddodamani635@gmail.com>
2026-03-14 19:16:39 +0530

    finding the numbers in java

--- vedio6/primenumbers.java
+++ vedio6/primenumbers.java
@@ -11,14 +11,13 @@ public class primenumbers {
             return;
         }
 
-        for (int i = 2; i <= n / 2; i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 System.out.println("not a prime number");
                 return;
             }
         }
 
-        // If loop completes, number is prime
         System.out.println("it is a prime number");
     }
 
@@ -26,7 +25,6 @@ public class primenumbers {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         printPrime(n);
+        sc.close();
     }
 }
-
-    
