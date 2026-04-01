54a422c2763d40b951b94579ca5706221728973a
shivanandDodamani <shivananddodamani635@gmail.com>
2026-04-01 22:53:32 +0530

    finding the sum of two given numbers

--- video2/javacode.java
+++ video2/javacode.java
@@ -1,13 +1,17 @@
 package video2;
 
+import java.util.Scanner;
+
 public class javacode {
     public static void main(String[] args) {
         // variables type 1
-        int a = 10;
-        int b = 20;
-        int sum = a+b;
-        System.out.println(sum);
-        
-    }
-      
-        }
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the first number:");
+    int a = sc.nextInt();
+    System.out.println("Enter the second number:");
+    int b = sc.nextInt();
+    int c = a+b;
+    System.out.println(c);
+    sc.close();
+}
+}
