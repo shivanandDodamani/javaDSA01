424124ef4982e03352759a5ed2b16e0c44e4b751
shivanandDodamani <shivananddodamani635@gmail.com>
2026-05-10 23:14:00 +0530

    bits in java

--- video12/bits.java
+++ video12/bits.java
@@ -6,16 +6,5 @@ public class bits {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        System.out.print("Enter a number ");
-        int n = sc.nextInt();
 
-        if(n % 2 == 0) {
-            System.out.println("the number is not prime ");
-        }else {
-             System.out.println("the number is  prime");
-            }
-         sc.close();
-
-    }
-}
     
