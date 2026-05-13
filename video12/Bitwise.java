05a1e36af0faf611b4fd6a2aaea901396b00a611
shivanandDodamani <shivananddodamani635@gmail.com>
2026-05-13 19:55:13 +0530

    Bitwise mask in java

--- video12/Bitwise.java
+++ video12/Bitwise.java
@@ -4,7 +4,7 @@ public class Bitwise {
     public static void main(String args[]){
         int n = 5;
         int pos = 3;
-        int bitMask = 1<<n;
+        int bitMask = 1<<pos;
 
         if((bitMask & n) == 0){
             System.out.println("bit is zero");
