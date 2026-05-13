fa0995a346f6d59d2006bb99cb51c85e1514f313
shivanandDodamani <shivananddodamani635@gmail.com>
2026-05-13 20:11:12 +0530

    bits.java

--- video12/bits.java
+++ video12/bits.java
@@ -4,7 +4,13 @@ import java.util.*;
 
 public class bits {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    int n = 5;
+    int pos = 1;
+    int bitMask = 1<<pos;
+
+    int newNumber = bitMask | n;
+    System.out.print(newNumber);
+
 
     }
 }
