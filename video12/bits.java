ba5416c3a390bbeac95b695599352a5a414680f5
shivanandDodamani <shivananddodamani635@gmail.com>
2026-05-15 22:58:13 +0530

    bits in java

--- video12/bits.java
+++ video12/bits.java
@@ -5,10 +5,11 @@ import java.util.*;
 public class bits {
     public static void main(String[] args) {
     int n = 5;
-    int pos = 1;
+    int pos = 2;
     int bitMask = 1<<pos;
+    int notBitMask = ~(bitMask);
 
-    int newNumber = bitMask | n;
+    int newNumber = notBitMask & n;
     System.out.print(newNumber);
     }
 }
